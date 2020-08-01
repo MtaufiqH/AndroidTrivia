b5d58505b8c455ca86f92dce08179bba6f8f2364
M. Taufiq Hidayat <mtaufiqh62@gmail.com>
2020-08-01 16:02:07 +0800

    set titleFragment to the activity_main.xml

--- app/src/main/java/com/example/androidtrivia/FragmentTitle.kt
+++ app/src/main/java/com/example/androidtrivia/FragmentTitle.kt
@@ -4,8 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-
+import com.example.androidtrivia.databinding.FragmentTitleBinding
 
 
 class FragmentTitle : Fragment() {
@@ -13,6 +14,11 @@ class FragmentTitle : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_title, container, false)
+
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
+            R.layout.fragment_title,container,false)
+
+
+        return binding.root
     }
 }
